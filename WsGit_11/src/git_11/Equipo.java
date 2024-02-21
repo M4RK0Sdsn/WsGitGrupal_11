4aa0ae3485e9d2e6d5a6c4fb943c8660390d7862
rubencit04 <tterubenator@gmail.com>
2024-02-21 10:22:26 +0100

    Equipo restante

--- WsGit_11/src/git_11/Equipo.java
+++ WsGit_11/src/git_11/Equipo.java
@@ -49,5 +49,63 @@ public class Equipo {
 			}
 			return existe;
 		}
+		// Los objetos tendrán la opción de devolver el numero de jugadores que tienen.
+		
+		public int devolverNumeroJugadores() {
+			int contador = 0;
+			for(int i = 0; i< this.listaNombres.length;i++) {
+				if(this.listaNombres[i] != null && this.listaNombres[i] != " ") {
+					contador++;
+				}
+			}
+			return contador;
+		}
+		
+		// Los objetos tendrán la opción de devolver si el número de jugadores que tienen es apto para jugar. 
+		// Un equipo es apto para jugar si tiene al menos 7 jugadores.
+		
+		public boolean esAptoParaJugar() {
+			int numeroJugadores = devolverNumeroJugadores();
+			boolean esApto = false;
+			if(numeroJugadores>7) {
+				esApto = true;
+			}else {
+				esApto = false;
+			}
+			return esApto;
+		}
+		
+		// Los objetos tendrán la opción de devolver si una lista de jugadores 
+		// pasada por parámetro es exactamente igual a su lista de jugadores
+		
+		public boolean esIgualListaJugadores(Equipo e) {
+			boolean esIgual = false;
+			for(int i = 0; i< this.listaNombres.length;i++) {
+					for(int j = 0; j< e.listaNombres.length ; j++) {
+						if(this.listaNombres[i].equalsIgnoreCase(e.listaNombres[j])) {
+							esIgual = true;
+						}	
+				}
+			}
+			return esIgual;
+		}
+		
+		// Los objetos tendrán la opción de devolver si un equipo pasado por 
+		// parámetro es exactamente igual al mismo. Un equipo es exactamente igual 
+		// si tiene el mismo nombre y la misma lista de jugadores con los mismos nombres.
+		
+		public boolean esIgualEquipo(Equipo e) {
+			boolean esIgual = false;
+			for(int i = 0; i< this.listaNombres.length;i++) {
+				if(this.listaNombres[i] == e.listaNombres[i] && this.nombre == e.nombre) {
+					esIgual = true;
+				}else {
+					esIgual = false;
+				}
+			}
+			return esIgual;
+			
+		}
+		
 
 }
